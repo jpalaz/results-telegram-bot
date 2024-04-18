c817683e78bd1acdad02519a58e78fac0ddb4616
Jan Pal <formula.pieramohi@gmail.com>
2024-04-18 19:03:43 +0300

    Fix ukr templates, remove redundant export.handler

--- functions/bot.js
+++ functions/bot.js
@@ -509,16 +509,6 @@ async function startStream() {
         }, socketFreq);
 }
 
-exports.handler = async event => {
-    try {
-        await bot.handleUpdate(JSON.parse(event.body))
-        return { statusCode: 200, body: "" }
-    } catch (e) {
-        console.error("error in handler:", e)
-        return { statusCode: 400, body: "This endpoint is meant for bot and telegram communication" }
-    }
-}
-
 console.log(`Starting streaming...`)
 startStream()
     .then(_ => {
